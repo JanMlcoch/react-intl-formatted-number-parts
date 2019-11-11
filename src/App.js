0d1f21c103e15193696f1879b92658b77e9f06e3
Jan Mlčoch <mlcoch.jan@gmail.com>
2019-11-11 11:02:25 +0100

    Add intl provider and formatted number parts example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FormattedNumberParts, IntlProvider } from 'react-intl';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={navigator.language}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>
+            <FormattedNumberParts value={123453} style='currency' currency={'CZK'}>
+              {parts => {
+                console.log(parts);
+                return parts.map(part => <span>{part.value}</span>)
+              }}
+            </FormattedNumberParts>
+          </div>
+
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
